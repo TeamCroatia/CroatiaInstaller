a9bc5868544a77565348038fbfc4130a5555f3e1
hoparkgo9ma <kpjhg0124@naver.com>
2016-02-27 09:00:06 +0900

    개발자용 임시 간편 설치기
    으으.. 이제야 커밋되네 붸에

---
+++ Extract.cmd
@@ -0,0 +1,2 @@
+7z x "%appdata%\.minecraft\Temp\GameLib.7z.001" "-o%appdata%\.minecraft" "-aoa"
+echo 압축 해제 완료.
---
+++ instend.cmd
@@ -0,0 +1,3 @@
+attrib +s +h "%appdata%\.minecraft\CroatiaInformation"
+"패치완료!.txt"
+del /q /s /f "%appdata%\.minecraft\CroatiaInformation"
